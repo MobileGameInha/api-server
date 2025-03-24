97a260e1784257a171638926acf5b2ffe3862c84
Haon <msung6924@naver.com>
2025-03-24 17:54:54 +0900

    chore: datasource 설정

--- src/main/java/cat/cat/member/presentation/MemberController.java
+++ src/main/java/cat/cat/member/presentation/MemberController.java
@@ -6,6 +6,12 @@ import cat.cat.member.dto.request.SignUpRequest;
 import cat.cat.member.dto.response.LoginResponse;
 import cat.cat.member.dto.response.MemberInfoResponse;
 import cat.cat.member.dto.response.SignUpResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,12 +22,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "member API", description = "유저 관련 API")
 @RequestMapping("/member")
 @RequiredArgsConstructor
 @RestController
 public class MemberController {
     private final MemberService memberService;
 
+    @Operation(summary = "회원가입")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공시", content = @Content(schema = @Schema(implementation = SignUpResponse.class)))})
     @PostMapping("/sign")
     public ResponseEntity<SignUpResponse> signUp(@RequestBody final SignUpRequest signUpRequest) {
         return ResponseEntity.ok(memberService.signUp(signUpRequest));
