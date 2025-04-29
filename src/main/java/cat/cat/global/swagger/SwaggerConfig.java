cd00ba29c66832e063ebe90b6a97b421bacc90ed
Haon <msung6924@naver.com>
2025-04-29 20:19:58 +0900

    fix: 의존성 충돌 문제 해결

--- src/main/java/cat/cat/global/swagger/SwaggerConfig.java
+++ src/main/java/cat/cat/global/swagger/SwaggerConfig.java
@@ -3,6 +3,7 @@ package cat.cat.global.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,10 +16,20 @@ public class SwaggerConfig {
                 .info(apiInfo());
     }
 
-    private Info apiInfo() {
+
+    public Info apiInfo() {
         return new Info()
                 .title("MBTIgram Springdoc")
                 .description("Springdoc을 사용한 MBTIgram Swagger UI")
                 .version("1.0.0");
+    }
+
+    // ★ 이게 반드시 필요함
+    @Bean
+    public GroupedOpenApi groupedOpenApi() {
+        return GroupedOpenApi.builder()
+                .group("v1") // swagger 그룹 이름
+                .pathsToMatch("/**") // 문서화할 경로
+                .build();
     }
 }
