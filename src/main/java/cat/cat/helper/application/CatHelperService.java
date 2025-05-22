fe0cd31e2532628e38d46c04563b2c78e8d3a7ec
Haon <msung6924@naver.com>
2025-05-22 22:46:34 +0900

    조력자랑 최대 만랩 체크

--- src/main/java/cat/cat/helper/application/CatHelperService.java
+++ src/main/java/cat/cat/helper/application/CatHelperService.java
@@ -84,6 +84,9 @@ public class CatHelperService {
 
     public void updateHelperLevel(final long memberId, final long helperId) {
         final CatHelper catHelper = catHelperRepository.findByMemberIdAndHelperId(memberId, helperId);
+        if(catHelper.getLevel() > 5) {
+            throw new CatHelperException("조력자의 최대 Level은 5입니다.");
+        }
         catHelper.setLevel(catHelper.getLevel() + 1);
     }
 }
