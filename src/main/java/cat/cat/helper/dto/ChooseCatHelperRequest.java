c71f88eadb0f3bf2c5440022ff4b657eff10b461
Haon <msung6924@naver.com>
2025-05-22 22:38:42 +0900

    조력자 선택 안되는 버그 수정

--- src/main/java/cat/cat/helper/dto/ChooseCatHelperRequest.java
+++ src/main/java/cat/cat/helper/dto/ChooseCatHelperRequest.java
@@ -3,12 +3,14 @@ package cat.cat.helper.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ChooseCatHelperRequest {
 
     @Schema(description = "전장에 데리고 갈 조력자 번호 리스트", example = "[1, 2, 5]")
-    private final List<Long> helperIds;
+    private List<Long> helperIds;
 
     public ChooseCatHelperRequest(final List<Long> helperIds) {
         this.helperIds = helperIds;
